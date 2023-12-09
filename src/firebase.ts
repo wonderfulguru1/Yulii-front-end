afe19fb1a4dbc17ffc9ec0be86514c5de4c26069
isaac <izik40004@gmail.com>
2023-12-09 02:24:18 +0100

    changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,15 +4,7 @@ import "firebase/auth";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCzRyP-rBzUbEX2C_kwu5rPYXbz8xfwA98",
-  authDomain: "intouch-dev-269817-de0b4.firebaseapp.com",
-  projectId: "intouch-dev-269817-de0b4",
-  storageBucket: "intouch-dev-269817-de0b4.appspot.com",
-  messagingSenderId: "907465139153",
-  appId: "1:907465139153:web:7e6e252986acc260d176f0",
-  measurementId: "G-CLYRMPJ5VB"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
