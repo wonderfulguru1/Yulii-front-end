5b1c9ecc37abf7d06d359f482eea72050aa8d2e9
Temiye Oluseun <temiye92@gmail.com>
2023-12-09 03:58:33 +0100

    updated env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+
+
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode,process.cwd(),'');
+  return{
+    define:{
+      'process.env':env
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [react()],
+  }
 })
