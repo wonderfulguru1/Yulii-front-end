fbbafdc4c63301b8260685fe7bac38c9d4ef0ead
Temiye Oluseun <temiye92@gmail.com>
2024-03-15 19:46:09 +0100

    Siteupdates

--- src/components/externalComponents/HeroSection.tsx
+++ src/components/externalComponents/HeroSection.tsx
@@ -1,12 +1,12 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import Button from './Button';
+/* import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Button from './Button'; */
 import Shape from './Shapes';
 import Shape2 from './Shape2';
 // import { useNavigate } from "react-router-dom";
 // import { useState } from "react";
 // import Toggle from './Toggle';
-import HeroImg from "../../assets/hero1.png"
-import { Translations } from '@/interface';
+/* import HeroImg from "../../assets/hero1.png"
+ */import { Translations } from '@/interface';
 import { useTranslation } from 'react-i18next';
 
 interface HeroProps {
@@ -31,25 +31,25 @@ interface HeroProps {
 // const coloredSentence = splitAndColorSentence(sentence);
 const HeroSection: React.FC<HeroProps> = () => {
     const { t } = useTranslation();
-   
-//   const coloredSentence = splitAndColorSentence(sentence);
+
+    //   const coloredSentence = splitAndColorSentence(sentence);
     // const imagePath = t(`homepage.sections.${sectionNa`)
 
-//     const navigate = useNavigate();
-//   const [isChecked, setIsChecked] = useState(false);
+    //     const navigate = useNavigate();
+    //   const [isChecked, setIsChecked] = useState(false);
 
-//   const handleToggle = () => {
-//     setIsChecked(!isChecked);
+    //   const handleToggle = () => {
+    //     setIsChecked(!isChecked);
 
-//     // Change the route based on the toggle state
-//     if (isChecked) {
-//         navigate("/home");
-//     } else {
-//         navigate("/merchants");
-//     }
-//   };
+    //     // Change the route based on the toggle state
+    //     if (isChecked) {
+    //         navigate("/home");
+    //     } else {
+    //         navigate("/merchants");
+    //     }
+    //   };
 
-//   const [isChecked, setIsChecked] = useState(false);
+    //   const [isChecked, setIsChecked] = useState(false);
 
 
     return (
@@ -57,62 +57,29 @@ const HeroSection: React.FC<HeroProps> = () => {
 
 
             <div className='flex text-center  container mx-auto items-center flex-col lg:py-12'>
-              
-              
-                <div className="flex items-center justify-center  mb-12">
-                    {/* <Toggle/> */}
-                    {/* <div className="flex items-center space-x-2">
-                    <Label htmlFor="airplane-mode">Airplane Mode</Label>
-      <Switch id="airplane-mode" />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
-    </div> */}
-                    {/* <label htmlFor="toggleB" className="flex items-center cursor-pointer">
-                        <div className="mr-6  font-semibold">
-                            FOR BUYERS
-                        </div>
-                        <div className="relative">
 
-                            <input type="checkbox" id="toggleB" className="sr-only" checked={isChecked} onChange={handleToggle}/>
-
-                            
-                            <div className="block border border-[#ea1d24] w-16 h-8 rounded-full bg-sec"></div>
-
-                            <div className="dot absolute left-1 top-1 bg-primary w-6 h-6 rounded-full transition"></div>
-                     
-                     
-                        </div>
- 
-
-                        <div className="ml-6  font-semibold">
-                            FOR MERCHANTS
-                        </div>
-                    </label> */}
-                </div>
                 <div className='flex md:flex-row flex-col justify-between lg:px-28'>
                     <div className='hidden lg:block'>
-                    <Shape />
+                        <Shape />
                     </div>
-                   
+
                     <div>
-                        <h1 className='lg:text-4xl text-xl font-semibold' data-aos="fade-right" > {t(`homepage.title1`)} <span className='text-[#67c7c0]'>{t(`homepage.title2`)} </span>{t(`homepage.title3`)} <span className='text-[#67c7c0]'>{t(`homepage.title4`)}</span></h1>
-                        <p className='py-10 text-center text-xl lg:px-64' data-aos="fade-left">{t(`homepage.subtitle`)}
-</p>
+                        <h1 className='intro-text lg:text-4xl text-xl font-semibold' data-aos="fade-right" 
+                        style={{fontSize: '4.25rem', lineHeight: '4.5rem'}}
+                        > {t(`homepage.title1`)} <span className='text-[#67c7c0]'>{t(`homepage.title2`)} </span>{t(`homepage.title3`)} <span className='text-[#67c7c0]'>{t(`homepage.title4`)}</span></h1>
+                        <p className='py-10 text-center text-xl lg:px-64' data-aos="fade-left" style={{paddingLeft: '0rem', paddingRight: '0rem'}}>{t(`homepage.subtitle`)}
+                        </p>
                     </div>
-                   
+
                     <div className='hidden lg:block'>
-                    <Shape2 />
+                        <Shape2 />
                     </div>
                 </div>
 
-                <Button title="Join for free" bgColor='bg-button1'/>
-                <LazyLoadImage
-                    src={HeroImg}
-                    // width={800}
-                    // height={600}
-                    alt="logo"
-                    className='pt-20'
-                    data-aos="zoom-out-right"
-                />
+                <iframe width="80%" height="400"
+                 src="https://www.youtube.com/embed/Mpw1quautoc?si=5TuiqjRB6c1OSnBz&autoplay=1;modestbranding=1&rel=0;controls=0;showinfo=0"
+                  title="YouTube video player" frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
 
             </div>
         </div>
